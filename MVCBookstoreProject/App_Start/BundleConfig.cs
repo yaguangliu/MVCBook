c18284f8e721a5e14b475c27c33586437a5a1bf6
yaguangliu <37131023+yaguangliu@users.noreply.github.com>
2022-10-10 17:55:22 -0400

    Update BundleConfig.cs

--- MVCBookstoreProject/App_Start/BundleConfig.cs
+++ MVCBookstoreProject/App_Start/BundleConfig.cs
@@ -18,6 +18,7 @@ namespace MVCBookstoreProject
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             // a new comment added by Yaguang
+            // another changes to the repo
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
